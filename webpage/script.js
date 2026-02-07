2a259543cfefd4e6941c639777dfe809d8043982
Lachlan Domst <lachlandomst@Lachlans-MacBook-Pro-2.local>
2026-02-07 12:27:11 -0800

    Updated script.js

--- webpage/script.js
+++ webpage/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://trailfeathers-github-io-2.onrender.com/";
+const API_BASE = "https://trailfeathers-github-io-real.onrender.com";
 
 // First Page
 const loginButton = document.querySelector("#login");
